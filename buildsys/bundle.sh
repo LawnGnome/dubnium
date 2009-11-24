7baf9c43af8b7c491a654fc02af70b4b22397dad
Adam Harvey <aharvey@php.net>
2009-11-24 22:36:27 +0800

    Further Mac fix.

--- buildsys/bundle.sh
+++ buildsys/bundle.sh
@@ -8,6 +8,7 @@ APP_BUNDLE="Dubnium.app"
 rm -rf "$TARGET"
 mkdir -p "$TARGET"
 cp -r "mac/$APP_BUNDLE/Contents" "$TARGET"
+mkdir -p "$TARGET/Contents/MacOS" >& /dev/null
 cp -r images/*png "$TARGET/Contents/Resources/"
 find "$TARGET" -name .svn -a -type d -exec rm -rf \{\} \; >& /dev/null
 cp "$BINARY" "$TARGET/Contents/MacOS/"
